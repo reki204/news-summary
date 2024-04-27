9628af000d3b26fec11e6034f6da448a2102511d
Onda Reki <rekionda0524@ezweb.ne.jp>
2024-04-27 12:18:41 +0900

    Article、responseのinterface定義

--- fetchNews.ts
+++ fetchNews.ts
@@ -1,6 +1,26 @@
 const NEWS_URL = 'https://newsapi.org/v2';
 
-export async function fetchTechnologyNews() {
+interface Article {
+  source: {
+    id: string | null;
+    name: string;
+  };
+  author: string | null;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string | null;
+  publishedAt: string;
+  content: string | null;
+}
+
+interface ApiResponse {
+  status: string;
+  totalResults: number;
+  articles: Article[];
+}
+
+export async function fetchTechnologyNews(): Promise<Article[]> {
   const params = {
     q: 'technology programming',
     language: 'en',
@@ -8,9 +28,10 @@ export async function fetchTechnologyNews() {
     apikey: Bun.env.API_KEY as string,
   }
   const url_query = new URLSearchParams(params);
+
   try {
     const response = await fetch(`${NEWS_URL}/everything?${url_query}`);
-    const data = await response.json();
+    const data: ApiResponse = await response.json();
     return data.articles;
   } catch (err) {
     console.error('Error fetching News:', err);
